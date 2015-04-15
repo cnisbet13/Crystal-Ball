ef2cbcc78da7d063a8d1dae3ebb61fde31d4ddea
Calvin Nisbet <cnisb044@uottawa.ca>
2015-04-15 14:50:15 -0700

    Final Commit

--- CrystalBall/CWNCrystalBall.m
+++ CrystalBall/CWNCrystalBall.m
@@ -15,14 +15,18 @@
 
     if (_predictions == nil){
         _predictions = [[NSArray alloc] initWithObjects:
-                        @"Yeah, right!",
-                        @"You Suck",
-                        @"Quit Pissing Me Off",
-                        @"Fuck, You're Fat",
-                        @"You're Fat",
-                        @"No Thanks",
-                        @"Chris Paul",
-                        @"Hobo", nil];
+                        @"Man will never reach the moon regardless of all future scientific advances.",
+                        @"The bomb will never go off. I speak as an expert in explosive.",
+                        @"There is no likehood man can ever tap the power of the atom.",
+                        @"Computers in the future may weigh no more than 1.5 tons.",
+                        @"I think there is a world market for maybe five computers.",
+                        @"640K ought to be enough for anybody.",
+                        @"This 'telephone' has too many shortcomings to be seriously considered as a means of communication. The device is inherently of no value to us.",
+                        @"Heavier-than-air flying machines are impossible.",
+                        @"Stocks have reached what looks like a permanently high plateau.",
+                        @"Everything that can be invented has been invented.",
+                        @"Louis Pasteur's theory of germs is ridiculous fiction.",
+                        nil];
     }
     return _predictions;
 
